bd6a380d3571aa2b1a8dec5cce56f1ea4fa7a789
Harish pirajin <harishharish0691@gmail.com>
2026-01-25 19:20:31 +0530

    add

---
+++ Q33.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    char a [20];
+    printf("Enter the str:");
+    scanf("%s",&a);
+    int b;
+    printf("Enter the value:");
+    scanf("%d",&b);
+    printf("%s %d\n",a,b);
+    return 0;
+
+}
