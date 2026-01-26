c8725bb2e1458927f2b1eb02cd4ac3afa98bf02e
Harish pirajin <harishharish0691@gmail.com>
2026-01-26 21:40:38 +0530

    add

---
+++ Q51.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main ()
+{
+ int a ;
+ printf("Enter the value of a:");
+ scanf("%d",&a);
+ if(a%2==0){
+    printf("it is a even:%d",a);
+
+ }else{
+    printf("it is a odd :%d",a);
+ }
+
+    return 0;
+}
