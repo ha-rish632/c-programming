9363dae6f75071bd1c798ed019334e3b14fc3249
Harish pirajin <harishharish0691@gmail.com>
2026-01-26 21:43:45 +0530

    add

---
+++ Q60.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+int main ()
+{
+int a,b,c;
+printf("enter the value:");
+scanf("%d %d %d",&a ,&b ,&c);
+if((a>b)&&(a>c)){
+printf("%d is lagest number",a);
+}else if((b>a)&&(b>c)){
+printf("%d is lagest number",b);
+}else{
+    printf("%d is lagest number",c);
+}
+
+    return 0;
+}
