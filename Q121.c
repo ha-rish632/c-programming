fc1fc5150967a8a6395712768254980230a66693
Harish pirajin <harishharish0691@gmail.com>
2026-01-28 21:49:07 +0530

    add

---
+++ Q121.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main ()
+{
+  int a;
+  scanf("%d",&a);
+   int  count =0;  
+  for ( int i=1;a>0;a/=10){
+       count ++;
+    }
+    printf("%d",count);
+    return 0;
+}
