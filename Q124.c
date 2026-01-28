397006c3ae92965110d44244f19353b110325288
Harish pirajin <harishharish0691@gmail.com>
2026-01-28 21:51:38 +0530

    add

---
+++ Q124.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main ()
+{
+    int i,p,n,result;
+    scanf("%d",&a,&b);
+  
+     for (i = 0; i <= p; i++) {
+        if (i == 0) continue;
+        result *= n;
+
+     }
+     printf("%d",result);
+    return 0;
+}
