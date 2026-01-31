9cc344ac7edc2380d7a4ec0d99d862df802546d4
Harish pirajin <harishharish0691@gmail.com>
2026-01-31 23:29:41 +0530

    add

---
+++ Q158.c
@@ -0,0 +1,24 @@
+  #include <stdio.h>
+
+int main()
+{
+    int fuel,n;
+    scanf("%d",&fuel);
+    scanf("%d",&n);
+
+    int i=0,x,count=0;
+
+    while(i<n)
+    {
+        scanf("%d",&x);
+        if(fuel<x)
+            break;
+        fuel-=x;
+        count++;
+        i++;
+    }
+
+    printf("Completed Trips: %d\n",count);
+    printf("Remaining Fuel: %d",fuel);
+    return 0;
+}
